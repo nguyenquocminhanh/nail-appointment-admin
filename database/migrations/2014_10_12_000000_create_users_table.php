eea5494154b96e43dc60dc78474dbebd9648d47e
minh <minhhanh.nguyen001@umb.edu>
2022-06-12 23:49:20 -0400

    Update migration visible default value

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
             $table->string('profile_image')->nullable();
-            $table->tinyInteger('visible')->comment('0=Not, 1=Visible');
+            $table->tinyInteger('visible')->default('1')->comment('0=Not, 1=Visible');
 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
